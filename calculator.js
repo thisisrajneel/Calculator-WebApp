38612773a0f69fae1a6689d7bf5601c13f51aa42
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-18 14:50:09 +0530

    basic implementation

--- calculator.js
+++ calculator.js
@@ -1,13 +1,37 @@
-const { ESRCH } = require('constants');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + "/index.html");
 })
 
 app.post('/', (req,res)=>{
-    res.send('thanks for using the calculator!')
+
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+    
+    if(req.body.add == 'on') {
+        var result = n1+n2;
+        res.send(`${n1} + ${n2} = ${result}`);
+    }
+    else if(req.body.sub == 'on') {
+        var result = n1-n2;
+        res.send(`${n1} - ${n2} = ${result}`);
+    }
+    else if(req.body.mul == 'on') {
+        var result = n1*n2;
+        res.send(`${n1} * ${n2} = ${result}`);
+    }
+    else if(req.body.div == 'on') {
+        var result = n1/n2;
+        res.send(`${n1} / ${n2} = ${result}`);
+    }
+    else {
+        alert('enter ONLY ONE operation!');
+    }
 })
 
 app.listen(3000, ()=>{
