2fee3e360698972e0a7f11b71d61e307eac7c01a
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-18 13:30:20 +0530

    responded with html doc

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send('hello world')
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.listen(3000, ()=>{
